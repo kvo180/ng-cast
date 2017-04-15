8561de4acb7bcf4658ab83d354c13e8d18827cb0
Hack Reactor Student <student@hackreactor.com>
2017-04-14 20:15:35 -0700

    Stored video data and added functionality to directive

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,21 @@
 angular.module('video-player')
 
+// .controller('appCtrl', function($scope) {
+//   $scope.videos = window.exampleVideoData;
+// })
+
 .directive('app', function() {
   return {
-    // TODO
+    scope: {
+      videos: '<'
+    },
+    restrict: 'E',
+    controllerAs: 'ctrl',
+    bindToController: true,
+    controller: function() {
+      this.videos = window.exampleVideoData;
+      console.log('APP: ', this.videos);
+    },
+    templateUrl: 'src/templates/app.html'
   };
 });
