a5611a96a4ad3e4be2af3a1ab010c21ba483bcaa
Hack Reactor Student <student@hackreactor.com>
2017-04-15 16:47:39 -0700

    Added debounce functionality

--- src/components/search.js
+++ src/components/search.js
@@ -1,11 +1,30 @@
 angular.module('video-player')
 
 .controller('SearchCtrl', function(youTube) {
+  this.debounce = (func, wait, immediate) => {
+    var timeout;
+    return () => {
+      var context = this, args = arguments;
+      var later = () => {
+        timeout = null;
+        if (!immediate) {
+          func.apply(context, args);
+        }
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) {
+        func.apply(context, args);
+      }
+    };
+  };
   this.search = (query, callback) => {
     youTube.search(query, callback);
-    this.input = '';
   };
+  this.debounce(this.search, 500, false);
   this.input = '';
+  this.service = {};
 })
 
 .directive('search', function() {
