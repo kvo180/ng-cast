e1bc59186d1b734ea84fd0d4258b999fe7b103f2
Hack Reactor Student <student@hackreactor.com>
2017-04-15 12:35:23 -0700

    Implemented http method to call YouTube API

--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,24 @@
 angular.module('video-player')
-.service('youTube', function(){
-  // TODO
+.service('youTube', function($http) {
+  this.search = (query, callback) => {
+    $http({
+      method: 'GET',
+      url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&key='+window.YOUTUBE_API_KEY+'&q='+query+'&maxResults=5&type=video&videoEmbeddable=true',
+      // params: 'part=snippet, type=video, videoEmbeddable=true, maxResults=5',
+      // headers: {'Authorization': window.YOUTUBE_API_KEY},
+      // data: {
+      //   part: 'snippet',
+      //   key: window.YOUTUBE_API_KEY,
+      //   q: query,
+      //   maxResults: 5,
+      //   type: 'video',
+      //   videoEmbeddable: true
+      // }
+    }).then(function successCallback(response) {
+      console.log('Success, response received');
+      callback(response.data.items);
+    }, function errorCallback(response) {
+      console.log('Error, request failed');
+    });
+  };
 });
