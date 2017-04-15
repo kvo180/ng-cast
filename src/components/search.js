76bae2e6164fc5d8a585f347402cd5ec48fffdba
Hack Reactor Student <student@hackreactor.com>
2017-04-15 12:30:54 -0700

    Impleted functionality to call YouTube API on button click

--- src/components/search.js
+++ src/components/search.js
@@ -2,6 +2,18 @@ angular.module('video-player')
 
 .directive('search', function() {
   return {
+    scope: {
+      selectVideo: '<'
+    },
+    restrict: 'E',
+    controllerAs: 'ctrl',
+    bindToController: true,
+    controller: function(youTube) {
+      this.result = (query, callback) => {
+        youTube.search(query, callback);
+      };
+      this.input = '';
+    },
     templateUrl: 'src/templates/search.html'
   };
 });
