c60c71c0424511408976e5fce744de5dd5beba4e
Hack Reactor Student <student@hackreactor.com>
2017-04-14 20:18:31 -0700

    Added functionality to directive

--- src/components/videoListEntry.js
+++ src/components/videoListEntry.js
@@ -1,6 +1,15 @@
 angular.module('video-player')
 .directive('videoListEntry', function() {
   return {
-    // TODO
+    scope: {
+      video: '<'
+    },
+    restrict: 'E',
+    controllerAs: 'ctrl',
+    bindToController: true,
+    controller: function() {
+      console.log('VIDOELISTENTRY', this);
+    },
+    templateUrl: 'src/templates/videoListEntry.html'
   };
 });
--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -2,6 +2,23 @@ angular.module('video-player')
 
 .directive('videoPlayer', function() {
   return {
-    // TODO
+    scope: {
+      video: '<',
+      text: '<'
+    },
+    restrict: 'E',
+    controllerAs: 'ctrl',
+    bindToController: true,
+    controller: function() {
+      console.log('PLAYER: ', this.video);
+      if (this.video) {
+        console.log('if');
+        this.text = '';
+      } else {
+        console.log('else');
+        this.text = 'Please wait';
+      }
+    },
+    templateUrl: 'src/templates/videoPlayer.html'
   };
 });
