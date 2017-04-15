7d01a04a990d6aec9d4d799a074811ccd5ba2bb9
Hack Reactor Student <student@hackreactor.com>
2017-04-14 20:17:39 -0700

    Added functionality to directive

--- src/components/videoList.js
+++ src/components/videoList.js
@@ -1,6 +1,19 @@
 angular.module('video-player')
 .directive('videoList', function() {
   return {
-    // TODO
+    scope: {
+      videos: '<',
+      onClick: '<'
+    },
+    restrict: 'E',
+    controllerAs: 'ctrl',
+    bindToController: true,
+    controller: function() {
+      this.onClick = (event) => {
+
+      };
+      console.log('VIDEOLIST', this);
+    },
+    templateUrl: 'src/templates/videoList.html'
   };
 });
