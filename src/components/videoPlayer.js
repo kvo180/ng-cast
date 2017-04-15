6537c6cf320ff603f78bb366ca2c94d01ada6e38
Hack Reactor Student <student@hackreactor.com>
2017-04-15 12:32:13 -0700

    Changed functionality to pass specs

--- src/components/videoPlayer.js
+++ src/components/videoPlayer.js
@@ -3,18 +3,13 @@ angular.module('video-player')
 .directive('videoPlayer', function() {
   return {
     scope: {
-      video: '<',
-      // text: '<' JUST DELETE THIS AND EVERYTHING WORKS!!!!?!?@?@?#@#$ 
+      video: '<'
     },
     restrict: 'E',
     controllerAs: 'ctrl',
     bindToController: true,
     controller: function() {
-      // Code below doesn't work: text will always be shown since this.video === undefined. WHY?@#
-      // if (!this.video) {
-      //   this.text = '';
-      // } 
-      // this.text = 'Please wait';
+      
     },
     templateUrl: 'src/templates/videoPlayer.html'
   };
