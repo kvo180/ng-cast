a4a9101d04da1821ef0afa2656b93935139d0f2c
Hack Reactor Student <student@hackreactor.com>
2017-04-15 12:30:11 -0700

    Added functionality to call YouTube API and initialize app

--- src/components/app.js
+++ src/components/app.js
@@ -6,12 +6,27 @@ angular.module('video-player')
 
 .directive('app', function() {
   return {
+    scope: {
+
+    },
     restrict: 'E',
     controllerAs: 'ctrl',
     bindToController: true,
-    controller: function() {
+    controller: function(youTube) {
       this.videos = window.exampleVideoData;
       this.currentVideo = this.videos[0];
+      this.onClick = ($event, index) => {
+        this.currentVideo = this.videos[index];
+      };
+      this.selectVideo = (videos) => {
+        this.videos = videos;
+        this.currentVideo = this.videos[0];
+      };
+      this.searchResults = (query, callback) => {
+        youTube.search(query, callback);
+      };
+      // initialize app by calling YouTube API for live data
+      this.searchResults('dogs', this.selectVideo);
     },
     templateUrl: 'src/templates/app.html'
   };
